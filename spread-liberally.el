76fd810fe89ed4e6df30abdbc20b57230d4787c6
glebite <glebite@gmail.com>
2021-01-03 14:12:03 -0500

    Added in actual code just for giggles.

---
+++ spread-liberally.el
@@ -0,0 +1,40 @@
+;; spread-liberally.el
+
+(defun kill-ring-hex-to-python-list ()
+  "Obviously bind this to something.
+  
+  tl;dr version:
+  I copy hex strings from wireshark to be used in developing python-based
+  dissectors.  The strings are of the form a1b2c3d4e5f6...  I need:
+  0xa1, 0xb2, 0xc3, 0xd4, 0xe5, 0xf6, ...  so I can complete things such
+  as: 
+
+  packet = [0xa1, 0xb2, 0xc3, 0xd4, 0xe5, 0xf6]
+
+  So when I'm in emacs land, I just like to copy, yank, execute my macro
+  and then move on.
+  "
+  (interactive)
+  (setq s (car kill-ring))
+  (setq counter 0)
+  (while (<  counter (length s))
+    (insert (format "0x%c%c, " (aref s counter) (aref s (+ 1 counter))))
+    (setq counter (+ counter 2))
+    )
+  )
+
+(defun double-to-single (beginning end)
+  "performs regsub of double quotes to single quotes
+   - probably done by another macro, but I am happy with
+     this for now.  May make it detect which quotes are 
+     present and force swap them - mainly for when I've dealt
+     with json stuff.
+  "
+  (interactive "*r")
+  (setq end (copy-marker end))
+    (save-excursion)
+    (goto-char beginning)
+    (while (re-search-forward "\\(\"\\)+" nil t)
+ 	(replace-match "'")
+ 	))
+
